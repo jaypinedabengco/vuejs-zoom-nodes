f914d3a8314e640b19a98f9e53dfec34f8e6b19e
Jay Bengco <jay.bengco@tooltwist.com>
2020-06-08 11:23:05 +0800

    Migrate code to ZoomNode

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Node from "@/components/ZoomNode/Node";
+import ZoomNode from "@/components/ZoomNode/ZoomNode";
 
 Vue.config.productionTip = false
 
 Vue.component('z-node', Node);
+Vue.component('z-zoom-node', ZoomNode);
 
 new Vue({
   router,
